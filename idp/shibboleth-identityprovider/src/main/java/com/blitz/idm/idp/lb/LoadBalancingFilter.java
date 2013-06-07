d16b907157d831e6fdde8faa24ed236e7cdd1238
Sergey <tadpoletad@gmail.com>
2013-06-07 18:29:52 +0400

    update

---
+++ idp/shibboleth-identityprovider/src/main/java/com/blitz/idm/idp/lb/LoadBalancingFilter.java
@@ -0,0 +1,42 @@
+package com.blitz.idm.idp.lb;
+
+import com.blitz.idm.idp.config.IdpConfig;
+import com.blitz.idm.idp.config.IdpConfigParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class LoadBalancingFilter implements Filter {
+    private static final Logger log = LoggerFactory.getLogger(LoadBalancingFilter.class);
+    private static boolean isLoadBalancingFilterEnabled;
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+      isLoadBalancingFilterEnabled = IdpConfig.getBooleanProperty(IdpConfigParam.IS_LOAD_BALANCING_ENABLED);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest shibRequest = (HttpServletRequest)servletRequest;
+        log.debug("LoadBalancingFilter.doFilter");
+
+        if(isLoadBalancingFilterEnabled ) {
+           log.debug("Load balancing enabled.");
+           shibRequest = new LBServletRequestWrapper((HttpServletRequest)servletRequest);
+           log.debug("Request modified.");
+        }
+        log.debug("request.getContextPath() = " + shibRequest.getContextPath());
+        log.debug("request.getQueryString() = " + shibRequest.getQueryString());
+        log.debug("request.getPathInfo() = " + shibRequest.getPathInfo());
+        log.debug("request.getServletPath() = " + shibRequest.getServletPath());
+        log.debug("request.getRequestURL() = " + shibRequest.getRequestURL().toString());
+        filterChain.doFilter(shibRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
