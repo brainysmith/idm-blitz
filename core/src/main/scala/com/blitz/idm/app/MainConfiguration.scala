4d9e69635bef717c492ca4a35028d796ce229d3a
volodia <brainysmith@gmail.com>
2013-07-23 11:17:47 +0400

    moving IdP to new configuration

--- core/src/main/scala/com/blitz/idm/app/MainConfiguration.scala
+++ core/src/main/scala/com/blitz/idm/app/MainConfiguration.scala
@@ -1,6 +1,8 @@
 package com.blitz.idm.app
 
 import com.typesafe.config.{Config, ConfigFactory}
+import java.net.URL
+import sun.plugin.dom.exception.InvalidStateException
 
 /**
  *
@@ -8,7 +10,14 @@ import com.typesafe.config.{Config, ConfigFactory}
 
 class NestedConfiguration(private val name: String)(implicit protected val parentConf: NestedConfiguration) {
 
-  protected val conf: Config = if(parentConf == null) ConfigFactory.load(name) else parentConf.conf.getConfig(name)
+  protected val conf: Config = if(parentConf == null) {
+    Option(System.getProperty("blitzConfUrl")).fold[Config](
+      throw new InvalidStateException("Property 'blitzConfUrl' is undefined.")
+    )(path => {
+      ConfigFactory.parseURL(new URL(path)).resolve()
+    })
+  }
+  else parentConf.conf.getConfig(name)
 
   def getSiblingConf(name: String) = parentConf.conf.getConfig(name)
 
--- idp/idp-config/src/test/scala/IdpConfigurationTest.scala
+++ idp/idp-config/src/test/scala/IdpConfigurationTest.scala
@@ -6,6 +6,8 @@ import org.specs2.mutable._
  */
 class IdpConfigurationTest extends Specification {
 
+  System.setProperty("blitzConfUrl", classOf[MainConfigurationTest].getResource("idm_blitz_idp.conf").toURI.toString)
+
   "Checking of configuration loader " should {
 
     "main-conf.data-dir must be set to '/opt/data'" in {
