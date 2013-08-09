d2445cc3e0cb42757f5a0248d3933bdc36f12c64
Sergey <tadpoletad@gmail.com>
2013-08-09 19:19:29 +0400

    test for web-login

--- web-login/test/ApplicationSpec.scala
+++ web-login/test/ApplicationSpec.scala
@@ -4,17 +4,30 @@ import org.specs2.mutable._
 
 import play.api.test._
 import play.api.test.Helpers._
+import com.blitz.idm.app._
+import play.api.test.FakeApplication
+import controllers.routes
 
 /**
  */
 class ApplicationSpec extends Specification {
-  
+
+  val FORM_URLENCODED_CONTENT_TYPE =  (CONTENT_TYPE -> "application/x-www-form-urlencoded")
+
   "Application" should {
 
     "basic login" in {
       running(FakeApplication()) {
-        val res = route(FakeRequest(POST, "/login/basic")).get
-        status(res) must equalTo(OK)
+        appLogTrace("Start basic login test")
+
+        val data: Map[String, Seq[String]] = Map("lgn" -> Seq("lgnTest"),
+                                                 "pswd" -> Seq("pswdTest"))
+
+        val request = FakeRequest(POST, routes.Login.basicLogin().url)
+        val result = route(request.withHeaders(FORM_URLENCODED_CONTENT_TYPE), data).get
+
+        appLogTrace("Get a result: {}", contentAsString(result))
+        status(result) must equalTo(OK)
       }
     }
     
