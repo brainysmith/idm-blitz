8a24452d4cb41d3a4edd2efb919f6d99ed414c66
vkarpov <brainysmith@gmail.com>
2013-08-27 18:05:37 +0400

    deserializer

--- core/src/main/scala/com/blitz/idm/app/json/JVal.scala
+++ core/src/main/scala/com/blitz/idm/app/json/JVal.scala
@@ -2,18 +2,24 @@ package com.blitz.idm.app.json
 
 import org.codehaus.jackson.map._
 import org.codehaus.jackson.`type`.JavaType
-import org.codehaus.jackson.{Version, JsonGenerator}
+import org.codehaus.jackson.{JsonToken, JsonParser, Version, JsonGenerator}
 import org.codehaus.jackson.map.annotate.JsonCachable
 import org.codehaus.jackson.map.module.SimpleModule
 import org.codehaus.jackson.map.Module.SetupContext
 import java.io.StringWriter
+import org.codehaus.jackson.map.`type`.TypeFactory
+import scala.collection.mutable
 
 
 /**
  *
  */
 trait JVal {
-  def toJson: String = JacksonBridge.jvalue2JsonString(this)
+  def toJson: String = JacksonBridge.jVal2JsonString(this)
+}
+
+object JVal {
+  def parseStr(str: String): JVal = JacksonBridge.jsonString2JVal(str)
 }
 
 case object JNull extends JVal
@@ -71,6 +77,151 @@ private[json] class JSerializer extends JsonSerializer[JVal]{
   }
 }
 
+class JObjStart
+object JObjStart extends JObjStart
+class JObjEnd
+object JObjEnd extends JObjEnd
+case class JObjField(name: String) //what about value
+
+class JArrStart
+object JArrStart
+class JArrEnd
+object JArrEnd
+
+trait JVisitor {
+  def visit(e: JObjStart)
+  def visit(e: JObjEnd)
+  def visit(e: JObjField)
+
+  def visit(e: JArrStart)
+  def visit(e: JArrEnd)
+
+  def visit(e: JNum)
+  def visit(e: JStr)
+  def visit(e: JBool)
+
+  def produce: JVal
+}
+
+class JValVisitor extends JVisitor {
+
+  //0 - Undefined, 1 - object, 2 - array, 3 - field
+  var level = 0
+  val levels = mutable.Stack[(Int, AnyRef)]()
+  var cur: AnyRef = null
+  var finalElem: JVal = _
+
+  private def popLevel() {
+    val old = cur
+    levels.pop() match {
+      case (l, c) => level = l; cur = c
+    }
+    old
+  }
+
+  def visit(e: JObjStart) {
+    if(level != 0) {
+      levels.push((level, cur))
+    }
+    cur = mutable.Seq[(String, JVal)]()
+    level = 1
+  }
+
+  def visit(e: JObjEnd) {
+    if(level != 1) {
+      throw new IllegalStateException("Malformed JSON. Called JObjEnd but current level is " + level)
+    }
+    val obj = JObj(cur.asInstanceOf[mutable.Seq[(String, JVal)]])
+    if(levels.isEmpty) {
+      finalElem = obj
+    }
+    else {
+      popLevel()
+      level match {
+        case 2 => cur.asInstanceOf[mutable.Seq[JVal]] :+ obj
+        case 3 =>
+        case (_ @ l, _) => throw new IllegalStateException("Malformed JSON. After JObjEnd on stack found level = " + l)
+      }
+    }
+
+  }
+
+  def visit(e: JObjField) {
+    if(level != 0) {
+      levels.push((level, cur))
+    }
+    cur = e.name
+    level = 3
+  }
+
+  def visit(e: JArrStart) {}
+
+  def visit(e: JArrEnd) {}
+
+  def visit(e: JNum) {
+    level match {
+      case 2 => cur.asInstanceOf[Seq[JVal]] :+ e
+      case 3 => {level = 1
+        cur = levels.pop() match {
+          case (1, s) => s.asInstanceOf[mutable.Seq[(String, JVal)]] :+ (cur, e)
+          case (_ @ l, _) => throw new IllegalStateException("Malformed JSON. After finishing filed on stack found level = " + l)
+        }
+      }
+    }
+  }
+
+  private def fieldEnd(v: JVal) {
+    val fieldName = popLevel()
+    if(level != 1)
+      throw new IllegalStateException("Mailformed JSON. After finishing a filed found level = " + level + " on the stack")
+    cur.asInstanceOf[mutable.Seq[(String, JVal)]] :+ (fieldName, v)
+  }
+
+  def visit(e: JStr) {}
+
+  def visit(e: JBool) {}
+
+  def produce: JVal = finalElem
+}
+
+@JsonCachable
+private[json] class JDeserializer(factory: TypeFactory, klass: Class[_]) extends JsonDeserializer[Object] {
+  def deserialize(jp: JsonParser, ctx: DeserializationContext): JVal = {
+
+    val visitor: JVisitor = new JValVisitor
+
+    if(!jp.hasCurrentToken)
+      jp.nextToken()
+    var tkn = jp.getCurrentToken
+
+    while(tkn != null) {
+      tkn match {
+        case JsonToken.NOT_AVAILABLE => throw new IllegalStateException("A JSON stream has finished unexpectedly")
+        case JsonToken.VALUE_EMBEDDED_OBJECT => throw new IllegalStateException("An embedded object found.")
+        case _ => accept(tkn, jp, visitor)
+      }
+      tkn = jp.nextToken()
+    }
+
+    val value = visitor.produce
+    if (!klass.isAssignableFrom(value.getClass)) {
+      throw ctx.mappingException(klass)
+    }
+    value
+  }
+
+  def accept(tkn: JsonToken, jp: JsonParser, visitor: JVisitor) {
+    tkn match {
+      case JsonToken.START_OBJECT => visitor.visit(JObjStart)
+      case JsonToken.FIELD_NAME => visitor.visit(new JObjField(jp.getCurrentName))
+      case JsonToken.END_OBJECT => visitor.visit(JObjEnd)
+      case JsonToken.VALUE_NUMBER_INT => visitor.visit(JNum(jp.getIntValue))
+      case _ => throw new IllegalStateException("Found unknown token " + tkn)
+    }
+  }
+
+}
+
 private[json] class JSerializers extends Serializers.Base {
   override def findSerializer(config: SerializationConfig, javaType: JavaType, beanDesc: BeanDescription, property: BeanProperty) =
     if(classOf[JVal].isAssignableFrom(beanDesc.getBeanClass)) {
@@ -81,6 +232,18 @@ private[json] class JSerializers extends Serializers.Base {
     }.asInstanceOf[JsonSerializer[Object]]
 }
 
+private[json] class JDeserializers extends Deserializers.Base {
+  override def findBeanDeserializer(javaType: JavaType, config: DeserializationConfig,
+                                    provider: DeserializerProvider, beanDesc: BeanDescription,
+                                    property: BeanProperty) = {
+    val klass = javaType.getRawClass
+    if (classOf[JVal].isAssignableFrom(klass)) {
+      new JDeserializer(config.getTypeFactory, klass)
+    } else null
+  }
+
+}
+
 private[json] object JacksonBridge {
 
   val mapper = new ObjectMapper
@@ -88,6 +251,7 @@ private[json] object JacksonBridge {
   object module extends SimpleModule("BlitzModule", new Version(1, 0, 0, null)) {
     override def setupModule(context: SetupContext) {
       context.addSerializers(new JSerializers)
+      context.addDeserializers(new JDeserializers)
     }
   }
   mapper.registerModule(module)
@@ -96,11 +260,17 @@ private[json] object JacksonBridge {
 
   private[this] def generator(output: StringWriter) = factory.createJsonGenerator(output)
 
-  private[json] def jvalue2JsonString(value: JVal): String = {
+  private[this] def jsonParser(s: String) = factory.createJsonParser(s)
+
+  private[json] def jVal2JsonString(value: JVal): String = {
     val writer = new StringWriter
     mapper.writeValue(generator(writer), value)
     writer.flush()
     writer.toString
+  }
+
+  private[json] def jsonString2JVal(strJson: String): JVal = {
+    mapper.readValue(jsonParser(strJson), classOf[JVal])
   }
 
 }
--- core/src/test/scala/JsonTest.scala
+++ core/src/test/scala/JsonTest.scala
@@ -24,6 +24,22 @@ class JsonTest extends Specification {
         )).toJson must be equalTo("{\"key1\":12,\"key2\":\"value2\",\"key3\":true,\"key4\":[10,\"value2\"],\"key5\":{\"key6\":17,\"key7\":37},\"key8\":null}")
       }
 
+      "deserialization" in {
+
+        val value = JVal.parseStr("{\"key\":{\"key2\":7}}")
+
+        println("Processed: " + value.toJson + "\n")
+
+        JObj(Seq("key1" -> JNum(12),
+          "key2" -> JStr("value2"),
+          "key3" -> JBool(true),
+          "key4" -> JArr(Array(JNum(10), JStr("value2"))),
+          "key5" -> JObj(Seq("key6" -> JNum(17),
+            "key7" -> JNum(37))),
+          "key8" -> JNull
+        )).toJson must be equalTo("{\"key1\":12,\"key2\":\"value2\",\"key3\":true,\"key4\":[10,\"value2\"],\"key5\":{\"key6\":17,\"key7\":37},\"key8\":null}")
+      }
+
     }
 
 }
